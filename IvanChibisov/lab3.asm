03f78b70489b84af1489bed6868d439d404c8ed2
IvanChibisov <43565389+IvanChibisov@users.noreply.github.com>
2018-10-01 00:25:45 +0300

    Добавил ЛР3(Чибисов И.В.)
    Задание аналогично ЛР2 , но с знаковыми числами.

--- IvanChibisov/lab3.asm
+++ IvanChibisov/lab3.asm
@@ -7,11 +7,15 @@
     d dw 0
     e dw 0
     i dw 0
+    sig dw 0
+    sig1 dw 0
+    sig2 dw 0
+    rez dw 0
     errorMessage1 db "Invalid input, uncorrect symbol,enter again$"
-    errorMessage2 db "Invalid input, Number>65536, enter again$"
+    errorMessage2 db "Invalid input, Number<-32768 AND Number>32767, enter again$"
     errorMessage3 db "Division by zero, the result is not obtained$"
 .code
-	PUTCHAR PROC
+	PUTCHARSIGNUM PROC
 		PUSH BX
 		PUSH AX
 		PUSH CX
@@ -19,8 +23,20 @@
 		MOV DX,0
 		MOV BX,0
 		MOV CX,0
-		MOV CX,10
+		SHL AX,1
+		JNC VVnext1
+		MOV DL, 45
+		MOV AH, 02h
+		INT 21h
+		MOV DX, 0
+		MOV AX,b
+		NEG AX
+		JMP vivod
+		VVnext1:
+		MOV DX, 0
+		MOV AX, b
 	vivod:
+		MOV CX,10
 		DIV CX
 		PUSH AX
 		PUSH DX
@@ -49,63 +65,100 @@
 		POP AX
 		POP BX
 		RET
-	PUTCHAR ENDP
+	PUTCHARSIGNUM ENDP
 	
-	GETCHAR PROC
+	GETCHARSIGNUM PROC
 		PUSH BX
 		PUSH AX
 		PUSH CX
 		MOV AX,0
-		MOV i, 0
+		MOV sig, AX
+		MOV i, AX
 		MOV BX,0
 		MOV CX,10
 		MOV b, 0
 	Vvod:
 		MOV AH, 01h
 		INT 21h
+		MOV CX,i
+		INC CX
+		MOV i,CX
 		MOV BL, 27
 		CMP AL, BL
-		JZ ESCape
+		JNZ next12
+		JMP ESCape
+	next12:
 		MOV BL, 8
 		CMP AL,BL
 		JNZ next1
 		JMP BackSpace
 		next1:
-		MOV BL,13
-		CMP AL,BL
-		JNZ next2
-		JMP ExitVvod
+			MOV BL,13
+			CMP AL,BL
+			JNZ next2
+			JMP ExitVvod
 		next2:
-		CMP AL,48
-		JNC next3
-		JMP Error
+			CMP AL,48
+			JNC next3
+			CMP AL, 45
+			JZ next5 
+			JMP Error
+		next5:
+			MOV rez, CX
+			MOV CX, i
+			CMP CX, 1
+			JZ SIGNUM
+			JMP Error 
+		SIGNUM:
+			MOV rez, CX
+			MOV CX, 1
+			MOV sig, CX
+			JMP Vvod	  	
 		next3:
-		MOV BL,57
-		CMP BL,AL
-		JNC next4
-		JMP Error
+			MOV BL,57
+			CMP BL,AL
+			JNC next4
+			JMP Error
 		next4:
-		MOV CX,i
-		INC CX
-		MOV i,CX
-		SUB AL, 48
-		MOV a,AL
-		MOV BL,1
-		MUL BL
-		MOV c,AX
-		MOV AX,b
-		MOV CX,10
-		MUL CX
-		JC Error2
-		ADD AX,c
-		JC Error2
-		MOV b,AX
-		JMP Vvod
+			SUB AL, 48
+			MOV a,AL
+			MOV BL,1
+			MUL BL
+			MOV c,AX
+			MOV AX,b
+			MOV CX,10
+			MUL CX
+			JNC next6
+			JMP Error2
+		next6:
+			ADD AX,c
+			JNC next11
+			JMP Error2
+		next11:
+			PUSH CX
+			MOV CX, 32768
+			CMP CX, AX
+			POP CX
+			JZ next10
+			JNC next7 
+			JMP Error2
+		next10:
+			PUSH CX
+			MOV CX, sig
+			CMP CX, 1
+			POP CX
+			JZ next7 
+			JMP Error2
+		next7: 
+			MOV b,AX
+			JMP Vvod
 	ESCape:
 		MOV AX, 0
 		MOV b,AX
 		MOV CX, i
 		INC CX
+		MOV i, AX
+		MOV sig, AX
 		cicleFor:
 			MOV DL, 8
 			MOV AH, 02h
@@ -138,8 +191,21 @@
 		MOV BX,0
 		MOV CX,10
 		MOV b, 0
+		MOV i,AX
+		MOV sig, AX
 		JMP Vvod 
 	BackSpace:
+		MOV CX,i
+		DEC CX
+		DEC CX
+		MOV i,CX
+		MOV CX,i
+		CMP CX, 0
+		JNZ next8
+		MOV CX, 2
+		MOV i, CX
+		JMP ESCape
+	next8:	
 		MOV CX,10
 		MOV DX,0
 		MOV AX,b
@@ -173,32 +239,44 @@
 		MOV BX,0
 		MOV CX,10
 		MOV b, 0
+		MOV i,AX
+		MOV sig, AX
 		JMP Vvod 
-		ExitVvod:
+	ExitVvod:
+		MOV AX, b
+		MOV CX, sig
+		CMP CX, 1
+		JNZ EXITT
+		NEG AX
+		MOV b, AX
+	EXITT:
 		POP CX
 		POP AX
 		POP BX
 		RET		
-	GETCHAR ENDP
+	GETCHARSIGNUM ENDP
 main:
     mov ax, @data
     mov ds, ax
     	
-	CALL GETCHAR
+	CALL GETCHARSIGNUM
 	MOV AX,b
 	MOV d, AX
-	CALL PUTCHAR
-	CALL GETCHAR
+	MOV b, AX
+	CALL PUTCHARSIGNUM
+	CALL GETCHARSIGNUM
 	MOV AX,b
 	MOV e, AX
-	CALL PUTCHAR
+	MOV b, AX
+	CALL PUTCHARSIGNUM
 	MOV AX, d
 	MOV BX, e
 	CMP BX, 0
 	JZ NULLdef
-	MOV DX,0
-	DIV BX
-	CALL PUTCHAR
+	CWD
+	IDIV BX
+	MOV b, AX
+	CALL PUTCHARSIGNUM
 	JMP exit
 	NULLdef:
 		MOV DL, 10
