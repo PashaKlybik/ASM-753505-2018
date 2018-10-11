7f8821603dd03e4212e881bfa1bc5209717f8a47
IvanChibisov <43565389+IvanChibisov@users.noreply.github.com>
2018-10-12 01:06:52 +0300

    Update IvanChibisov/lab1.asm
    Выровнял

--- IvanChibisov/lab1.asm
+++ IvanChibisov/lab1.asm
@@ -9,43 +9,40 @@
 main:
     mov ax, @data
     mov ds, ax
-    
-		mov bx, a
-		mov cx, b
-		mov dx, c
-		CMP bx, cx
-		JNC bigger1
-		CMP bx, dx
-		JNC bigger2
-		mov dx, d
-		CMP bx, dx
-		JNC bigger3
-		mov ax, a
-		JMP exit
-	bigger3:
-		mov ax, d
-		JMP exit
-	bigger2:
-		mov bx, d
-		CMP dx,bx
-		JNC bigger3
-	MaxC:
-		mov ax, c
-		JMP exit
-	bigger1:
-		CMP cx, dx
-		JNC bigger4
-		mov dx, d
-		CMP cx, dx
-		JNC bigger3
-		mov ax, b
-		JMP exit
-	bigger4:
-		JMP bigger2
-		JMP exit
-    
-	exit:
-
+mov bx, a
+mov cx, b
+mov dx, c
+CMP bx, cx
+JNC bigger1
+CMP bx, dx
+JNC bigger2
+mov dx, d
+CMP bx, dx
+JNC bigger3
+mov ax, a
+JMP exit
+bigger3:
+mov ax, d
+JMP exit
+bigger2:
+mov bx, d
+CMP dx,bx
+JNC bigger3
+MaxC:
+mov ax, c
+JMP exit
+bigger1:
+CMP cx, dx
+JNC bigger4
+mov dx, d
+CMP cx, dx
+JNC bigger3
+mov ax, b
+JMP exit
+bigger4:
+JMP bigger2
+JMP exit
+exit:
     mov ax, 4c00h
     int 21h
 end main
