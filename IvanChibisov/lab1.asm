e19f41d31c8217049e71715a39c9988e5ec78096
IvanChibisov <43565389+IvanChibisov@users.noreply.github.com>
2018-10-12 10:55:11 +0300

    Update IvanChibisov/lab1.asm
    Добавил схему алгоритма

--- IvanChibisov/lab1.asm
+++ IvanChibisov/lab1.asm
@@ -12,23 +12,23 @@ main:
 mov bx, a
 mov cx, b
 mov dx, c
-CMP bx, cx
-JG bigger1
-CMP bx, dx
-JG bigger2
-mov dx, d
-CMP bx, dx
-JG bigger3
-mov ax, a
-JMP exit
-bigger3:
-mov ax, d
-JMP exit
-bigger2:
-mov bx, d
-CMP dx,bx
-JG bigger3
-MaxC:
+CMP bx, cx		; algorithm diagram
+JG bigger1              ;if (a>b)
+CMP bx, dx		;	if (b>c)
+JG bigger2		;		if (c>d)
+mov dx, d		;			min=d
+CMP bx, dx		;		else min=c
+JG bigger3		;	else    if (b>d)
+mov ax, a		;			min=d
+JMP exit		;		else min=b
+bigger3:		;else   if (a>c)
+mov ax, d		;		if (c>d)
+JMP exit		;			min=d
+bigger2:		;		else min=c
+mov bx, d		;	else    if (a>d)
+CMP dx,bx		;			min=d
+JG bigger3		;		else min=a
+MaxC:			
 mov ax, c
 JMP exit
 bigger1:
