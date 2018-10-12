d50a1a56b2e121e0d0f4db3857be201c733e986d
VadimOliseichik <43565670+VadimOliseichik@users.noreply.github.com>
2018-10-12 11:11:40 +0300

    Update VadimOliseichik/Lab1.asm
    Исправил.

--- VadimOliseichik/Lab1.asm
+++ VadimOliseichik/Lab1.asm
@@ -1,53 +1,53 @@
 .model small
 .stack 256
 .data
-    a dw 4
-    b dw 65
-    c dw 2 
-    d dw 1
+    a dw 3
+    b dw 13
+    c dw 14 
+    d dw 17
 .code
 main:
 mov ax, @data
 mov ds, ax
 
-mov ax, [a]
-mul ax 
-mul ax 
-mov cx, ax
-mov ax, [b]
-mul ax 
-mov bx, ax 
-cmp cx, bx 
-jc label1 
+ mov ax, [a]
+ mul ax 
+ mul [a]
+ mov cx, ax
+ mov ax, [b]
+ mul ax 
+ mov bx, ax 
+ cmp cx, bx 
+ jc label1 
 
-mov ax, [c] 
-mov dx, [d] 
-mul dx
-mov bx, ax 
-mov ax, [a] 
-mov cx, [b]
-div cx 
-mov dx, ax 
-cmp bx, dx
-jz label2
+ mov ax, [c] 
+ mov dx, [d] 
+ mul dx
+ mov bx, ax 
+ mov ax, [a] 
+ mov cx, [b]
+ div cx 
+ mov dx, ax 
+ cmp bx, dx
+ jz label2
     
-mov ax, [c]
-jmp labelfinish
+ mov ax, [c]
+ jmp labelfinish
 
-label2: 
-mov ax, [a] 
-mov cx, [b] 
-and ax, cx 
-jmp labelfinish
+ label2: 
+ mov ax, [a] 
+ mov cx, [b] 
+ and ax, cx 
+ jmp labelfinish
 
-label1:
-mov ax, [c] 
-mov dx,[d] 
-mul dx 
-add ax,[b] 
-jmp labelfinish
+ label1:
+ mov ax, [c] 
+ mov dx,[d] 
+ mul dx 
+ add ax,[b] 
+ jmp labelfinish
 
-labelfinish:
+ labelfinish:
 mov ax, 4c00h
 int 21h
 end main
