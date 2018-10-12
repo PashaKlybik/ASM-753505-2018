7f4dc5b9c997fec733e75554760047687d95417d
VadimOliseichik <43565670+VadimOliseichik@users.noreply.github.com>
2018-10-12 11:14:37 +0300

    Update VadimOliseichik/Lab1.asm
    Исправил.

--- VadimOliseichik/Lab1.asm
+++ VadimOliseichik/Lab1.asm
@@ -10,44 +10,45 @@ main:
 mov ax, @data
 mov ds, ax
 
- mov ax, [a]
- mul ax 
- mul [a]
- mov cx, ax
- mov ax, [b]
- mul ax 
- mov bx, ax 
- cmp cx, bx 
- jc label1 
+    mov ax, [a]
+    mul ax 
+    mul [a]
+    mov cx, ax
+    mov ax, [b]
+    mul ax 
+    mov bx, ax 
+    cmp cx, bx 
+    jc label1 
 
- mov ax, [c] 
- mov dx, [d] 
- mul dx
- mov bx, ax 
- mov ax, [a] 
- mov cx, [b]
- div cx 
- mov dx, ax 
- cmp bx, dx
- jz label2
+    mov ax, [c] 
+    mov dx, [d] 
+    mul dx
+    mov bx, ax 
+    mov ax, [a] 
+    mov cx, [b]
+    div cx 
+    mov dx, ax 
+    cmp bx, dx
+    jz label2
     
- mov ax, [c]
- jmp labelfinish
+    mov ax, [c]
+    jmp labelfinish
 
- label2: 
- mov ax, [a] 
- mov cx, [b] 
- and ax, cx 
- jmp labelfinish
+    label2: 
+    mov ax, [a] 
+    mov cx, [b] 
+    and ax, cx 
+    jmp labelfinish
 
- label1:
- mov ax, [c] 
- mov dx,[d] 
- mul dx 
- add ax,[b] 
- jmp labelfinish
+    label1:
+    mov ax, [c] 
+    mov dx,[d] 
+    mul dx 
+    add ax,[b] 
+    jmp labelfinish
 
- labelfinish:
+    labelfinish:
+
 mov ax, 4c00h
 int 21h
 end main
