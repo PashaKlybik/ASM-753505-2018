491f348f986a82bcebee5f40813dab9e6dc45f36
Zviger <36892110+Zviger@users.noreply.github.com>
2018-10-15 18:49:34 +0300

    Update Zviger/lab1.asm

--- Zviger/lab1.asm
+++ Zviger/lab1.asm
@@ -11,66 +11,64 @@
         Иначе
            Результат = c % d + b * a
 
-
 .model small
 .stack 256
 .data
     a dw 14
     b dw 58
-	c dw 24
-	d dw 18
+    c dw 24
+    d dw 18
 .code
 main:
     mov ax, @data
     mov ds, ax
     
-	mov AX,a
-	mov BX,b
-
-	mov DI, AX
-	inc DI
-	or AX, DI
-	cmp AX, BX
-
-	JNZ flag1
-
-	mov AX, a
-	mul BX
-	add CX, D
-	mov CX, d
-	div CX
-	mov AX, DX
-	jmp exit
-
-	flag1:
-	
-	mov AX, a
-	and AX, BX
-	mov CX, c
-	or CX, d
-	cmp AX,CX
-
-	JNZ flag2
-
-	mov DI, BX
-	dec DI
-	and BX, DI
-	mov AX, BX
-	jmp exit
-
-	flag2:
-
-	mov AX, c
-	mov DX, 0
-	div d
-
-
-	mov CX,DX
-	mov AX, a
-	mul BX
-	add AX,CX
-
-	exit:
+    mov AX,a
+    mov BX,b
+    
+    mov DI, AX
+    inc DI
+    or AX, DI
+    cmp AX, BX
+    
+    JNZ flag1
+    
+    mov AX, a
+    mul BX
+    add CX, D
+    mov CX, d
+    div CX
+    mov AX, DX
+    jmp exit
+    
+    flag1:
+    
+    mov AX, a
+    and AX, BX
+    mov CX, c
+    or CX, d
+    cmp AX,CX
+    
+    JNZ flag2
+    
+    mov DI, BX
+    dec DI
+    and BX, DI
+    mov AX, BX
+    jmp exit
+    
+    flag2:
+    
+    mov AX, c
+    mov DX, 0
+    div d
+    
+    mov CX,DX
+    mov AX, a
+    mul BX
+    add AX,CX
+    
+    exit:
     mov ax, 4c00h
     int 21h
 end main
