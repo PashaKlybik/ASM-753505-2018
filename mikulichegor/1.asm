5aced6dfa4e62691e5a33bc241c04bb977aeb3fe
mikulichegor <43569200+mikulichegor@users.noreply.github.com>
2018-10-26 10:26:01 +0300

    Update 1.asm

--- mikulichegor/1.asm
+++ mikulichegor/1.asm
@@ -1,29 +1,28 @@
 .model small
- .stack 256
+.stack 256
 .data
-  
-  a dw 100
-  b dw -93
-  c dw -57
-  d dw 55
+    a dw 100
+    b dw -93
+    c dw -57
+    d dw 55
 .code
- main:
-  
-  mov ax, a
-  cmp ax, b
-  jge first
-   mov ax, b
- first:
-   cmp ax,c
-   jge second
-   mov ax,c
+main:
+    mov ax, a
+    cmp ax, b
+    jge first
+    mov ax, b
+ 
+first:
+    cmp ax,c
+    jge second
+    mov ax,c
+
 second:
-   cmp ax,d
-   jge third
-   mov ax,d
+    cmp ax,d
+    jge third
+    mov ax,d
+
 third:
- int 21h
-    mov ax, 4c00h
-   
+    mov ax, 4c00h  
     int 21h
     end main 
