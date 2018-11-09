ba0e0306e7b302456c0157cb35f1615e75a00fa3
justme9999 <just.me.99@yandex.ru>
2018-11-09 10:44:55 +0300

    [Oleg Zhdanenya]LR3
    Задание:
    1) написать процедуру для вывода знакового числа размерностью слово из регистра AX на экран
       на входе число в регистре AX
       на выходе это число на экране
       все задействованные регистры должны быть сохранены в процедуре
    2) написать процедуру для ввода знакового числа с клавиатуры в регистр AX
       на входе пользовательский ввод, минус может быть только как первый символ
       на выходе введенное число в регистре AX
       все задействованные регистры должны быть сохранены в процедуре (кроме AX)
       также должна быть проверка вводимого числа на правильность:
          нельзя вводить не цифры, минус может быть только первым символом,
          проверка на допустимые значения [-32768;32767]
       дополнительно можно реализовать обработку клавиши бэкспэйс (удалить один
          последний введенный символ) и клавиши ескэйп (удалить все число).
    3) в функции main действия, аналогичные предыдущей лабораторной работе, но для знаковых чисел.

---
+++ OlegZhdanenya/lab3.asm
@@ -0,0 +1,245 @@
+.model small
+.stack 256
+.data
+    len db 0
+    sign db 0
+    border dw 32768
+    dividend dw ?
+    divisor dw ?
+    ten dw 10
+    errorMesssage db "Error$"
+    resultMessage db "Result: $"
+    remainderMessage db "Remainder: $"
+.code
+
+WriteMessage proc
+    push ax
+    mov ah, 09h
+    int 21h
+    pop ax
+    ret
+WriteMessage endp
+
+setsign:
+    inc sign
+    inc border
+    jmp Input
+
+ReadProc proc
+    push bx
+    push cx
+    push dx
+    push border
+    xor bx, bx
+    xor dx, dx
+    mov ax, 0
+    mov sign, al
+FirstInput:
+    mov ah, 01h
+    int 21h
+    cmp al, '-'
+    je setsign
+    cmp al, 13
+    jne nextstep1
+    jmp exit
+nextstep1:
+    cmp al, 8
+    je backspace
+    cmp al, '0'
+    jnc nextstep3
+    jmp ErrChar
+nextstep3:
+    cmp al, '9'+1
+    jnc ErrChar
+    sub al, '0'
+    xor ah, ah
+    mov bx, ax
+    inc len
+    jmp Input
+Input:
+    mov ah, 01h
+    int 21h
+    cmp al, 13
+    jne nextstep2
+    jmp exit
+nextstep2:
+    cmp al, 8
+    je backspace
+    cmp al, '0'
+    jc ErrChar
+    cmp al, '9'+1
+    jnc ErrChar
+    sub al, '0'
+    xor ch, ch
+    mov cl, al
+    mov ax, bx
+    xor dx, dx
+    mul ten
+    cmp dx, 1
+    jnc ErrChar
+    cmp ax, border
+    jnc ErrChar
+    add ax, cx
+    cmp ax, border
+    jnc ErrChar
+    mov bx, ax
+    inc len
+    jmp Input
+
+backspace:
+    cmp len, 0
+    je revsign
+    mov dl, ' '
+    mov ah, 02h
+    int 21h
+    mov dl, 8
+    mov ah, 02h
+    int 21h
+    mov ax, bx
+    xor dx, dx
+    div ten
+    mov bx, ax
+    dec len
+    cmp len, 0
+    je revsign
+    jmp Input
+
+ErrChar:
+    mov dl, 8
+    mov ah, 02h
+    int 21h
+    mov dl, ' '
+    mov ah, 02h
+    int 21h
+    mov dl, 8
+    mov ah, 02h
+    int 21h
+    jmp Input
+
+revsign:
+    pop border
+    push border
+    mov ax, 0
+    mov sign, al
+    mov dl, ' '
+    mov ah, 02h
+    int 21h
+    mov dl, 8
+    mov ah, 02h
+    int 21h
+    jmp FirstInput
+
+reverse:
+    neg bx
+    jmp next
+    
+exit:
+    cmp sign, 1
+    jnc reverse
+next:
+    mov ax, 0
+    mov sign, al
+    mov ax, bx
+    pop border
+    pop dx
+    pop cx
+    pop bx
+    ret
+ReadProc endp
+
+WriteProc proc
+    push bx
+    push cx
+    push dx
+    xor bx, bx
+    cmp ax, border
+    jnc showsign
+DivCycle:
+    xor dx, dx
+    div ten
+    push dx
+    inc bx
+    mov cx, ax
+    inc cx
+    loop DivCycle
+    mov cx, bx
+Output:
+    pop dx
+    add dx, '0'
+    mov ah, 02h
+    int 21h
+    loop Output
+    mov dl, 10
+    mov ah, 02h
+    int 21h
+    pop dx
+    pop cx
+    pop bx
+    ret
+showsign:
+    push ax
+    mov dl, '-'
+    mov ah, 02h
+    int 21h
+    pop ax
+    neg ax
+    jmp DivCycle
+WriteProc endp
+
+start:
+    mov ax, @data
+    mov ds, ax
+
+    mov ax, dividend
+    call ReadProc
+    mov dividend, ax
+    call WriteProc
+    call ReadProc
+    mov divisor, ax
+    call WriteProc
+    xor dx, dx
+    mov ax, dividend
+    cmp ax, border
+    cmp divisor, 0
+    je caseerror
+    cmp divisor, -1
+    je error
+nextstep4:
+    cwd
+    idiv divisor
+    cmp dx, border
+    jnc changerem
+nextstep5:
+    call WriteProc
+    mov ax, dx
+    lea dx, remainderMessage
+    call WriteMessage
+    call WriteProc
+    jmp toend
+
+changerem:
+    mov bx, divisor
+    cmp bx, border
+    jc decquot
+    sub dx,bx
+    inc ax
+    jmp nextstep5
+decquot:
+    add dx, bx
+    dec ax
+    jmp nextstep5
+    
+error:
+    mov cx, border
+    cmp dividend, cx
+    jc nextstep4
+caseerror:
+    lea dx, errorMesssage
+    mov ah, 09h
+    int 21h
+toend:
+    mov ah, 01h
+    int 21h
+    mov ax, 4c00h
+    int 21h
+end start
