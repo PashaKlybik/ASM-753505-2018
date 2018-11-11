7c6a7aac7f02a0e9a5bb60ee75e47a2c507f937e
khavlaren <khavlaren@gmail.com>
2018-11-11 13:14:43 +0300

    Исправил пробелы.

--- khavlaren/lab4.asm
+++ khavlaren/lab4.asm
@@ -2,32 +2,32 @@
 .386
 .stack 100h
 .data
-	maxLine db 254
-	len db 0
+    maxLine db 254
+    len db 0
     line db 254 dup('$')
-	
-	messageMax db "The longest word is $"
-	messageMin db "The shortest word is $"
-	newLine db 10, 13, '$'
-	
-	maxLength dw 0
-	minLength dw 254
-	maxIndex dw 0
-	minIndex dw 0
-	
-	minWord db 100 dup('$')
-	maxWord db 100 dup('$')
+    
+    messageMax db "The longest word is $"
+    messageMin db "The shortest word is $"
+    newLine db 10, 13, '$'
+    
+    maxLength dw 0
+    minLength dw 254
+    maxIndex dw 0
+    minIndex dw 0
+    
+    minWord db 100 dup('$')
+    maxWord db 100 dup('$')
 .code
 
 PrintNewLine PROC
-	PUSH AX
-	PUSH DX
-	LEA DX, newLine
-	MOV AH, 09h
-	INT 21h
-	POP DX
-	POP AX
-	RET
+    PUSH AX
+    PUSH DX
+    LEA DX, newLine
+    MOV AH, 09h
+    INT 21h
+    POP DX
+    POP AX
+    RET
 PrintNewLine ENDP
 
 Output PROC
@@ -54,8 +54,8 @@ OutCycle:
     INT 21h
     LOOP OutCycle
     
-	CALL PrintNewLine
-	
+    CALL PrintNewLine
+    
     POP DX
     POP CX
     POP BX
@@ -68,178 +68,170 @@ main:
     MOV AX, @data
     MOV DS, AX
     MOV ES, AX
-	
-	; Input.
-	LEA DX, maxLine
-	MOV AH, 0Ah
-	INT 21h
-	CALL PrintNewLine
-	LEA DX, line
-	MOV AH, 09h
-	INT 21h
-	CALL PrintNewLine
-	MOV AL, len
-	XOR AH, AH
-	CALL Output
-	; End Input.
-	
-	; --------------------------------------------------------------------------------
-	
-	; Processing.
-	LEA SI, line
-	XOR CH, CH
-	MOV CL, len					; Счётчик длины строки (текущий индекс).
-	; MOV minLength, CX
-	XOR BX, BX					; Длина текущего слова.
-	CLD
+    
+    ; Input.
+    LEA DX, maxLine
+    MOV AH, 0Ah
+    INT 21h
+    CALL PrintNewLine
+    LEA DX, line
+    MOV AH, 09h
+    INT 21h
+    CALL PrintNewLine
+    MOV AL, len
+    XOR AH, AH
+    CALL Output
+    
+    ; Processing.
+    LEA SI, line
+    XOR CH, CH
+    MOV CL, len					; Счётчик длины строки (текущий индекс).
+    XOR BX, BX					; Длина текущего слова.
+    CLD
 StringCycle:
-	LODSB						; В AL текущий символ, переход на следующий символ.
-	;
-	CMP AL, ' '
-	JZ BlankSpace
-	CMP AL, '.'
-	JZ BlankSpace
-	CMP AL, ','
-	JZ BlankSpace
-	; CMP AL, '-'
-	; JZ BlankSpace
-	CMP AL, ':'
-	JZ BlankSpace
-	CMP AL, '!'
-	JZ BlankSpace
-	CMP AL, '?'
-	JZ BlankSpace
-	CMP AL, '"'
-	JZ BlankSpace
-	; CMP AL, '\''
-	; JZ BlankSpace
-	CMP AL, '('
-	JZ BlankSpace
-	CMP AL, ')'
-	JZ BlankSpace
-	;
-	INC BX
-	DEC CX
-	JCXZ Exit
-	JMP StringCycle
-	
+    LODSB						; В AL текущий символ, переход на следующий символ.
+    CMP AL, ' '
+    JZ BlankSpace
+    CMP AL, '.'
+    JZ BlankSpace
+    CMP AL, ','
+    JZ BlankSpace
+    CMP AL, ':'
+    JZ BlankSpace
+    CMP AL, '!'
+    JZ BlankSpace
+    CMP AL, '?'
+    JZ BlankSpace
+    CMP AL, '"'
+    JZ BlankSpace
+    CMP AL, 027h				; '
+    JZ BlankSpace
+    CMP AL, '('
+    JZ BlankSpace
+    CMP AL, ')'
+    JZ BlankSpace
+    
+    INC BX
+    DEC CX
+    JCXZ Exit
+    JMP StringCycle
+    
 BlankSpace:
-	XOR AH, AH
-	MOV AL, len
-	SUB AX, CX
-	SUB AX, BX
-	CMP BX, maxLength
-	JA MaxLengthChanged
+    XOR AH, AH
+    MOV AL, len
+    SUB AX, CX
+    SUB AX, BX
+    CMP BX, maxLength
+    JA MaxLengthChanged
 LabelCheckForMinimal:
-	CMP BX, minLength
-	JB MinLengthChanged
+    CMP BX, minLength
+    JB MinLengthChanged
 RestartWord:
-	XOR BX, BX
+    XOR BX, BX
 StillBlank:
-	LODSB
-	DEC CX
-	JCXZ Exit
-	;
-	CMP Al, ' '
-	JZ StillBlank
-	CMP Al, '.'
-	JZ StillBlank
-	CMP Al, ','
-	JZ StillBlank
-	; CMP Al, '-'
-	; JZ StillBlank
-	CMP Al, ':'
-	JZ StillBlank
-	CMP Al, '!'
-	JZ StillBlank
-	CMP Al, '?'
-	JZ StillBlank
-	CMP Al, '('
-	JZ StillBlank
-	CMP Al, ')'
-	JZ StillBlank
-	CMP Al, '"'
-	JZ StillBlank
-	; CMP Al, '\''
-	; JZ StillBlank
-	;
-	DEC SI
-	JMP StringCycle
-	; End Processing.
-	
+    LODSB
+    DEC CX
+    JCXZ Exit
+    ;
+    CMP Al, ' '
+    JZ StillBlank
+    CMP Al, '.'
+    JZ StillBlank
+    CMP Al, ','
+    JZ StillBlank
+    CMP Al, '-'
+    JZ StillBlank
+    CMP Al, ':'
+    JZ StillBlank
+    CMP Al, '!'
+    JZ StillBlank
+    CMP Al, '?'
+    JZ StillBlank
+    CMP Al, '('
+    JZ StillBlank
+    CMP Al, ')'
+    JZ StillBlank
+    CMP Al, '"'
+    JZ StillBlank
+    CMP Al, 027h
+    JZ StillBlank
+    
+    DEC SI
+    JMP StringCycle
+    ; End Processing.
+    
 MaxLengthChanged:
-	MOV maxIndex, AX
-	MOV maxLength, BX
-	JMP LabelCheckForMinimal
-	
+    MOV maxIndex, AX
+    MOV maxLength, BX
+    JMP LabelCheckForMinimal
+    
 MinLengthChanged:
-	MOV minIndex, AX
-	MOV minLength, BX
-	JMP RestartWord
-	
+    MOV minIndex, AX
+    MOV minLength, BX
+    JMP RestartWord
+    
 Exit:
-	XOR AH, AH
-	MOV AL, len
-	SUB AX, BX
-	CMP BX, maxLength
-	JB MaxNotChanged
-	MOV maxIndex, AX
-	MOV maxLength, BX
-	JMP DisplayResult
+    XOR AH, AH
+    MOV AL, len
+    SUB AX, BX
+    CMP BX, maxLength
+    JB MaxNotChanged
+    MOV maxIndex, AX
+    MOV maxLength, BX
+    JMP DisplayResult
 MaxNotChanged:
-	CMP BX, 0
-	JZ DisplayResult
-	CMP BX, minLength
-	JA DisplayResult
-	MOV minIndex, AX
-	MOV minLength, BX
-	
+    CMP BX, 0
+    JZ DisplayResult
+    CMP BX, minLength
+    JA DisplayResult
+    MOV minIndex, AX
+    MOV minLength, BX
+    
 DisplayResult:	
-	
-	; Setting strings.
-	LEA SI, line
-	LEA DI, minWord
-	ADD SI, minIndex
-	MOV CX, minLength
-	REP MOVSB
-	CALL PrintNewLine
-	LEA SI, line
-	LEA DI, maxWord
-	ADD SI, maxIndex
-	MOV CX, maxLength
-	REP MOVSB
-	; End Setting strings.
+    
+    ; Setting strings.
+    LEA SI, line
+    LEA DI, minWord
+    ADD SI, minIndex
+    MOV CX, minLength
+    REP MOVSB
+    CALL PrintNewLine
+    LEA SI, line
+    LEA DI, maxWord
+    ADD SI, maxIndex
+    MOV CX, maxLength
+    REP MOVSB
 
-	LEA DX, messageMin
-	MOV AH, 09h
-	INT 21h
-	CALL PrintNewLine
-	LEA DX, minWord
-	MOV AH, 09h
-	INT 21h
-	CALL PrintNewLine
-	MOV AX, minIndex
-	CALL Output
-	MOV AX, minLength
-	CALL Output
-	
-	CALL PrintNewLine
-	LEA DX, messageMax
-	MOV AH, 09h
-	INT 21h
-	CALL PrintNewLine
-	LEA DX, maxWord
-	MOV AH, 09h
-	INT 21h
-	CALL PrintNewLine
-	MOV AX, maxIndex
-	CALL Output
-	MOV AX, maxLength
-	CALL Output
-	
-	CALL PrintNewLine
+    LEA DX, messageMin
+    MOV AH, 09h
+    INT 21h
+    CALL PrintNewLine
+    LEA DX, minWord
+    MOV AH, 09h
+    INT 21h
+    CALL PrintNewLine
+    MOV AX, minIndex
+    CALL Output
+    MOV AX, minLength
+    CALL Output
+    
+    CALL PrintNewLine
+    LEA DX, messageMax
+    MOV AH, 09h
+    INT 21h
+    CALL PrintNewLine
+    LEA DX, maxWord
+    MOV AH, 09h
+    INT 21h
+    CALL PrintNewLine
+    MOV AX, maxIndex
+    CALL Output
+    MOV AX, maxLength
+    CALL Output
+    
+    CALL PrintNewLine
 
-	MOV ax, 4c00h
-	INT 21h
+    MOV ax, 4c00h
+    INT 21h
 
 end main
