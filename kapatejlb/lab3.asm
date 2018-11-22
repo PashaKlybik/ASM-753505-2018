6e981fe1ff958d3ea64ca1bcb775f772d18d58fb
kapatejlb <43680951+kapatejlb@users.noreply.github.com>
2018-11-22 19:07:34 +0300

    [Карницкий Владислав] Лабораторная Работа №3
    исправление багов

--- kapatejlb/lab3.asm
+++ kapatejlb/lab3.asm
@@ -7,52 +7,46 @@ devider dw 0
 temporary dw 0
 
 currentpos dw ?
-
-tempsign dw ?
-signfirst dw ? ;если 1, то +, а если 0, то -
-signsecond dw ?
+isnegative dw ?
 
 enterdevident db "Enter devident: ", '$'
 enterdevider db "Enter devider: ", '$'
 repeat db 13, 10,"Repeat please!", 13, 10, '$'
-wholerespos db "Result: ", '$'
-wholeresneg db "Result: -", '$'
-fractional db ", fractional = $"
+wholeres db "Result: ", '$'
 errorzero db "Division by zero!", 13, 10, '$'
 n db 10,"$"
 u dw 10
-ost dw ?
 cel dw ?
 
 .code
 
 InputInt proc	;ввод в АX 
-
-	mov currentpos, 0 
+	mov currentpos, 0
+	mov isnegative, 0
 
 	entersymb:
+		add currentpos, 1
+
 		MOV AH, 01h
 		INT 21h
 		xor ah, ah
-
-		add currentpos, 1
 
 		cmp ax, 43
 		jz plus
 
 		cmp ax, 45
-		jz minus
+		jz minus	
 
-	usuallyint:
-	CMP AX, 13 ;проверка на энтр
-	JZ end1
-	CMP AX, 48 ;проверка на 0-9
-	JC error
-	CMP AX, 57
-	JZ continusiallyint
-	JNC error
+	usiallint:
+		CMP AX, 13 ;проверка на энтр
+		JZ end1
+		CMP AX, 48 ;проверка на 0-9
+		JC error
+		CMP AX, 57
+		JZ usiallint
+		JNC error
 
-	continusiallyint:
+	contin:
 		SUB AL, 48
 		MOV AH, 0
 		MOV BX, AX
@@ -65,16 +59,22 @@ InputInt proc	;ввод в АX
 		JMP entersymb
 
 	plus:
-		mov tempsign, 1
 		cmp currentpos, 1
-		jz entersymb
+		jz positive
 		jmp error
 
+	positive:
+		mov isnegative, 0
+		jmp entersymb
+
 	minus:
-		mov tempsign, 0
 		cmp currentpos, 1
-		jz entersymb
+		jz negative
 		jmp error
+
+	negative:
+		mov isnegative, 1
+		jmp entersymb
 
 	error:
 		mov currentpos, 0
@@ -85,28 +85,50 @@ InputInt proc	;ввод в АX
 		MOV temporary, AX
 		JMP entersymb
 
+	makeneg:
+		neg temporary
+		jmp end2
+
 	end1:
+		cmp isnegative, 1
+		jz makeneg
+
+	end2:
+
 	ret
 InputInt endp
 
-OutputInt proc
-	MOV CX, 0
-	next:
-		MOV DX, 0
-		DIV u
-		PUSH DX
-		MOV DX, 0
-		INC CX
-		CMP AX, 0
-		JNZ next
+OutputInt proc ;вывод из ах
 
-	cycle:
-		POP DX
-		MOV DH, 0
-		ADD DL, 48
-		MOV AH, 02h
-		INT 21h
-		LOOP cycle
+	test ax, ax ;; Проверяем число на знак.
+	jns out1
+
+	mov  cx, ax ;; Если оно отрицательное, выведем минус и оставим его модуль.
+    mov ah, 02h
+    mov dl, '-'
+    int 21h
+    mov ax, cx
+    neg ax
+
+    out1:
+		MOV CX, 0
+
+		next:
+			MOV DX, 0
+			DIV u
+			PUSH DX
+			MOV DX, 0
+			INC CX
+			CMP AX, 0
+			JNZ next
+
+		cycle:
+			POP DX
+			MOV DH, 0
+			ADD DL, 48
+			MOV AH, 02h
+			INT 21h
+			LOOP cycle
 
 	ret
 OutputInt endp
@@ -125,74 +147,57 @@ stasyan proc
 	ret
 stasyan endp
 
-start: 
-MOV AX, @data
-MOV DS, AX
+start:
+	MOV AX, @data
+	MOV DS, AX
 
-LEA DX, enterdevident
-MOV AH, 09h
-INT 21h
-call InputInt
-push temporary
-pop devident
-mov temporary, 0
+	LEA DX, enterdevident
+	MOV AH, 09h
+	INT 21h
+	call InputInt
 
-mov ax, tempsign
-mov signfirst, ax
+	push temporary
+	pop devident
+	mov ax, devident
+	mov temporary, 0
 
-LEA DX, enterdevider
-MOV AH, 09h
-INT 21h
-call InputInt
-push temporary
-pop devider
-mov temporary, 0
+	LEA DX, enterdevider
+	MOV AH, 09h
+	INT 21h
+	call InputInt
 
-mov ax, tempsign
-mov signsecond, ax
+	push temporary
+	pop devider
+	mov ax, devider
+	mov temporary, 0
 
-cmp devider, 0
-jz altendofprog
+	cmp devider, 0
+	jz altendofprog
 
-mov ax, devident
-cwd
-div devider
+	mov ax, devident
+	cwd
+	idiv devider
 
-mov cel, ax ;ax=целое
-mov ost, dx ;dx=остаток
+	mov cel, ax ;ax=целое
+	mov ost, dx ;dx=остаток
 
-mov ax, signfirst
-cmp ax, signsecond
-jz ansplus
-jnz ansminus
+	LEA DX, wholeres
+	MOV AH, 09h
+	INT 21h
+	mov ax, cel
 
-ansplus:
-LEA DX, wholeresneg
-MOV AH, 09h
-INT 21h
-mov ax, cel
-call outputint
-jmp enddd
+	call outputint
 
-ansminus:
-LEA DX, wholerespos
-MOV AH, 09h
-INT 21h
-mov ax, cel
-call outputint
-jmp enddd
+	call stasyan
 
-enddd:
-call stasyan
+	MOV AH, 4Ch
+	INT 21h
 
-MOV AH, 4Ch
-INT 21h
+	altendofprog:
+	LEA DX, errorzero
+	MOV AH, 09h
+	INT 21h
 
-altendofprog:
-LEA DX, errorzero
-MOV AH, 09h
-INT 21h
-
-MOV AH, 4Ch
-INT 21h
+	MOV AH, 4Ch
+	INT 21h
 end start 
