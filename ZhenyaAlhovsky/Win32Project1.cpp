7d72553e8258478b38be35419f05ef7af53a2b2e
ZhenyaAlhovsky <ZhenyaAlh@users.noreply.github.com>
2018-12-04 08:09:59 +0300

    Lab 7

---
+++ ZhenyaAlhovsky/Win32Project1.cpp
@@ -0,0 +1,249 @@
+// Win32Project1.cpp: определяет точку входа для приложения.
+//
+
+#include "stdafx.h"
+#include "Win32Project1.h"
+
+#define MAX_LOADSTRING 100
+
+// Глобальные переменные:
+HINSTANCE hInst;                                // текущий экземпляр
+WCHAR szTitle[MAX_LOADSTRING];                  // Текст строки заголовка
+WCHAR szWindowClass[MAX_LOADSTRING];            // имя класса главного окна
+HWND hWnd2;
+int X = 0, Y = 0;
+bool todraw = false;
+
+// Отправить объявления функций, включенных в этот модуль кода:
+ATOM                MyRegisterClass(HINSTANCE hInstance);
+BOOL                InitInstance(HINSTANCE, int);
+LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
+INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM);
+void DrawSmthCool(HDC hdc, int x, int y, bool if_false_do_nothing);
+
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
+                     _In_opt_ HINSTANCE hPrevInstance,
+                     _In_ LPWSTR    lpCmdLine,
+                     _In_ int       nCmdShow)
+{
+    UNREFERENCED_PARAMETER(hPrevInstance);
+    UNREFERENCED_PARAMETER(lpCmdLine);
+
+    // TODO: разместите код здесь.
+
+    // Инициализация глобальных строк
+    LoadStringW(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
+    LoadStringW(hInstance, IDC_WIN32PROJECT1, szWindowClass, MAX_LOADSTRING);
+    MyRegisterClass(hInstance);
+
+    // Выполнить инициализацию приложения:
+    if (!InitInstance (hInstance, nCmdShow))
+    {
+        return FALSE;
+    }
+
+    HACCEL hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_WIN32PROJECT1));
+
+    MSG msg;
+
+    // Цикл основного сообщения:
+    while (GetMessage(&msg, nullptr, 0, 0))
+    {
+        if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
+        {
+            TranslateMessage(&msg);
+            DispatchMessage(&msg);
+        }
+    }
+
+    return (int) msg.wParam;
+}
+
+
+
+//
+//  ФУНКЦИЯ: MyRegisterClass()
+//
+//  НАЗНАЧЕНИЕ: регистрирует класс окна.
+//
+ATOM MyRegisterClass(HINSTANCE hInstance)
+{
+    WNDCLASSEXW wcex;
+
+    wcex.cbSize = sizeof(WNDCLASSEX);
+
+    wcex.style          = CS_HREDRAW | CS_VREDRAW;
+    wcex.lpfnWndProc    = WndProc;
+    wcex.cbClsExtra     = 0;
+    wcex.cbWndExtra     = 0;
+    wcex.hInstance      = hInstance;
+    wcex.hIcon          = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_WIN32PROJECT1));
+    wcex.hCursor        = LoadCursor(nullptr, IDC_ARROW);
+    wcex.hbrBackground  = (HBRUSH)(COLOR_WINDOW+1);
+    wcex.lpszMenuName   = MAKEINTRESOURCEW(IDC_WIN32PROJECT1);
+    wcex.lpszClassName  = szWindowClass;
+    wcex.hIconSm        = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
+
+    return RegisterClassExW(&wcex);
+}
+
+//
+//   ФУНКЦИЯ: InitInstance(HINSTANCE, int)
+//
+//   НАЗНАЧЕНИЕ: сохраняет обработку экземпляра и создает главное окно.
+//
+//   КОММЕНТАРИИ:
+//
+//        В данной функции дескриптор экземпляра сохраняется в глобальной переменной, а также
+//        создается и выводится на экран главное окно программы.
+//
+BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
+{
+   hInst = hInstance; // Сохранить дескриптор экземпляра в глобальной переменной
+
+   HWND hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
+      CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, nullptr, nullptr, hInstance, nullptr);
+
+   if (!hWnd)
+   {
+      return FALSE;
+   }
+
+   ShowWindow(hWnd, nCmdShow);
+   UpdateWindow(hWnd);
+
+   return TRUE;
+}
+
+//
+//  ФУНКЦИЯ: WndProc(HWND, UINT, WPARAM, LPARAM)
+//
+//  НАЗНАЧЕНИЕ:  обрабатывает сообщения в главном окне.
+//
+//  WM_COMMAND — обработать меню приложения
+//  WM_PAINT — отрисовать главное окно
+//  WM_DESTROY — отправить сообщение о выходе и вернуться
+//
+//
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	switch (message)
+	{
+	case WM_CREATE:
+		{
+		hWnd2 = CreateWindowEx(NULL,
+			L"BUTTON",
+			L"Clear",
+			WS_TABSTOP | WS_VISIBLE |
+			WS_CHILD | BS_DEFPUSHBUTTON,
+			100,
+			100,
+			100,
+			24,
+			hWnd,
+			HMENU(IDC_MyButton),
+			hInst,
+			NULL);
+		ShowWindow(hWnd2, 1);
+		break;
+		}
+	case WM_LBUTTONDOWN:
+		{
+			int iPosX = LOWORD(lParam);
+			int iPosY = HIWORD(lParam);
+			todraw = true;
+			X = iPosX;
+			Y = iPosY;
+			
+			InvalidateRect(hWnd, NULL, FALSE);
+			break;
+		}
+    case WM_COMMAND:
+        {
+            int wmId = LOWORD(wParam);
+			
+
+            // Разобрать выбор в меню:
+            switch (wmId)
+            {
+			case IDC_MyButton:              //clear
+				todraw = false;
+				InvalidateRect(hWnd, NULL, TRUE);
+				break;
+            case IDM_ABOUT:
+                DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
+                break;
+            case IDM_EXIT:
+                DestroyWindow(hWnd);
+                break;
+            default:
+                return DefWindowProc(hWnd, message, wParam, lParam);
+            }
+        }
+        break;
+    case WM_PAINT:
+        {
+			PAINTSTRUCT ps;	
+			HDC hdc = BeginPaint(hWnd, &ps);
+			DrawSmthCool(hdc, X, Y, todraw);
+			EndPaint(hWnd, &ps);
+        }
+        break;
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        break;
+    default:
+        return DefWindowProc(hWnd, message, wParam, lParam);
+    }
+    return 0;
+}
+
+// Обработчик сообщений для окна "О программе".
+INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    UNREFERENCED_PARAMETER(lParam);
+    switch (message)
+    {
+    case WM_INITDIALOG:
+        return (INT_PTR)TRUE;
+
+    case WM_COMMAND:
+        if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
+        {
+            EndDialog(hDlg, LOWORD(wParam));
+            return (INT_PTR)TRUE;
+        }
+        break;
+    }
+    return (INT_PTR)FALSE;
+}
+
+void DrawSmthCool(HDC hdc, int x, int y, bool if_false_do_nothing)
+{
+	if (!if_false_do_nothing) return;
+	HPEN hPenOld;
+	HPEN hlinePen;
+	COLORREF qLineColor = RGB(90, 90, 90);
+	hlinePen = CreatePen(PS_SOLID, 5, qLineColor);
+	hPenOld = (HPEN)SelectObject(hdc, hlinePen);
+	MoveToEx(hdc, x + 50, y + 50, NULL);
+	LineTo(hdc, x + 50, y - 50);
+	LineTo(hdc, x - 50, y - 50);
+	LineTo(hdc, x - 50, y + 50);
+	LineTo(hdc, x + 50, y + 50);
+	MoveToEx(hdc, x, y - 100, NULL);
+	LineTo(hdc, x + 70, y - 50);
+	LineTo(hdc, x - 70, y - 50);
+	LineTo(hdc, x , y - 100);
+	MoveToEx(hdc, x + 20, y + 20, NULL);
+	LineTo(hdc, x + 20, y - 20);
+	LineTo(hdc, x - 20, y - 20);
+	LineTo(hdc, x - 20, y + 20);
+	LineTo(hdc, x + 20, y + 20);
+	MoveToEx(hdc, x, y + 20, NULL);
+	LineTo(hdc, x, y - 20);
+	MoveToEx(hdc, x + 20, y, NULL);
+	LineTo(hdc, x - 20, y);
+	SelectObject(hdc, hPenOld);
+	DeleteObject(hlinePen);
+}
