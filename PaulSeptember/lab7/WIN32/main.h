92bdccfb2983ffc860d99e28af7e1abbe5637848
Sanguine <PavelKamenko@mail.ru>
2018-12-15 04:11:04 +0300

    init commit
    Signed-off-by: Sanguine <PavelKamenko@mail.ru>

---
+++ PaulSeptember/lab7/WIN32/main.h
@@ -0,0 +1,10 @@
+#define CM_FILE_SAVEAS	9072
+#define CM_FILE_EXIT	9071
+#define CM_FILE_OPEN	9070
+#define CM_ABOUT        9069
+#define CLOSE 101
+#define SWITCH 111
+#define CLEAR 1000
+
+
+#define IDC_MAIN_TEXT 1001
